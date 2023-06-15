3940730628d953b17a734eada83bf2f6278e626b
Alan Cho <alancho397@gmail.com>
2023-06-15 18:56:53 -0500

    Added solution four

--- Easy/easyOne.js
+++ Easy/easyOne.js
@@ -19,10 +19,35 @@ function logOddNumbers() {
 // logOddNumbers();
 
 // 3)
+
 function logEvenNumbers() {
   for (let i = 2; i <= 98; i += 2) {
     console.log(i);
   }
 }
 
-logEvenNumbers();
+// logEvenNumbers();
+
+// 4)
+/*
+const readline = require("readline-sync");
+const METER_TO_FEET_CONVERSION = 10.7639;
+let length = readline.question(
+  "Please enter the length of the room in meters: "
+);
+let width = readline.question("Please enter the width of the room in meters: ");
+
+function calculateRoomArea(length, width) {
+  let areaSqMeters = length * width;
+  let areaSqFeet = length * width * METER_TO_FEET_CONVERSION;
+  console.log(
+    `The area of your room is ${areaSqMeters.toFixed(
+      2
+    )} square meters or ${areaSqFeet.toFixed(2)} square feet.`
+  );
+}
+
+calculateRoomArea(length, width);
+*/
+
+// 4)
