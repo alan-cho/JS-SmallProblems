708b9b7698c97fac8189dc45ba9f05b690d22097
Alan Cho <alancho397@gmail.com>
2023-06-17 04:08:51 -0500

    Add solution eleven

--- Easy/easyTwo.js
+++ Easy/easyTwo.js
@@ -161,3 +161,18 @@ console.log(retire(initialAge, finalAge));
 */
 
 // 11)
+/*
+function centerOf(string) {
+  if (string.length % 2 === 0) {
+    return string[string.length / 2 - 1] + string[string.length / 2];
+  } else {
+    return string[Math.floor(string.length / 2)];
+  }
+}
+
+console.log(centerOf("I Love JavaScript")); // "a"
+console.log(centerOf("Launch School")); // " "
+console.log(centerOf("Launch")); // "un"
+console.log(centerOf("Launchschool")); // "hs"
+console.log(centerOf("x")); // "x"
+*/
