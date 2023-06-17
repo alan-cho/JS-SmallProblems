0ac23cc2b5d2d164b7282c1cdd09858b66a36bc0
Alan Cho <alancho397@gmail.com>
2023-06-17 04:39:29 -0500

    Add solution one

---
+++ Easy/easyThree.js
@@ -0,0 +1,24 @@
+// 1)
+/*
+function crunch(string) {
+  let result = "";
+  let uniqueChar = "";
+  for (let i = 0; i <= string.length - 1; i += 1) {
+    if (uniqueChar !== string[i]) {
+      result += string[i];
+      uniqueChar = string[i];
+    } else {
+      continue;
+    }
+  }
+  return result;
+}
+
+console.log(crunch("ddaaiillyy ddoouubbllee"));
+console.log(crunch("4444abcabccba"));
+console.log(crunch("ggggggggggggggg"));
+console.log(crunch("a"));
+console.log(crunch(""));
+*/
+
+// 2)
