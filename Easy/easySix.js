4f42571a23257e48333a2101b21e1616352556e4
Alan Cho <alancho397@gmail.com>
2023-06-27 17:52:20 -0500

    Updated problem set easySix.js, added solution four

--- Easy/easySix.js
+++ Easy/easySix.js
@@ -13,4 +13,21 @@ repeater("Good job!"); // "GGoooodd  jjoobb!!"
 repeater(""); // ""
 */
 
-// 2)
+// 2 - 3) SKIPPED
+
+// 4)
+/*
+function sequence(integer) {
+  let result = [];
+  for (let i = 1; i <= integer; i += 1) {
+    result.push(i);
+  }
+  console.log(result);
+}
+
+sequence(5); // [1, 2, 3, 4, 5]
+sequence(3); // [1, 2, 3]
+sequence(1); // [1]
+*/
+
+// 5)
