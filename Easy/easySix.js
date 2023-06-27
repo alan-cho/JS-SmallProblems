768ab40126fd3af5c38a5d26d7480ef4c3dada73
Alan Cho <alancho397@gmail.com>
2023-06-27 17:37:58 -0500

    Create file easySix.js, added solution one

---
+++ Easy/easySix.js
@@ -0,0 +1,16 @@
+// 1)
+/*
+function repeater(string) {
+  let result = "";
+  for (let index in string) {
+    result += string[index] + string[index];
+  }
+  console.log(result);
+}
+
+repeater("Hello"); // "HHeelllloo"
+repeater("Good job!"); // "GGoooodd  jjoobb!!"
+repeater(""); // ""
+*/
+
+// 2)
