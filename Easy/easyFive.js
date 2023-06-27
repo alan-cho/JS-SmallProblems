bedc9efebfd3d78f4981bd5a502ea1d453d8a3d8
Alan Cho <alancho397@gmail.com>
2023-06-27 14:45:49 -0500

    Add solution four

--- Easy/easyFive.js
+++ Easy/easyFive.js
@@ -159,3 +159,53 @@ halvsies([]); // [[], []]
 */
 
 //4)
+/*
+Understand the Problem
+  Input: Array
+  Output: Integer
+
+  Explicit:
+    Given a random, unordered array containing integers only.
+    There will be only one integer that occurs twice.
+    Find the duplicate integer.
+
+  Implicit:
+    There is no limit to how large the array is.
+
+Data Structures
+  Array
+
+Algorithms
+  Create a copy of the input array.
+  Iterate through the copied array.
+  Count the amount of duplicates there are for an element when iterating through the copied array.
+  If the count amount is 2, return that element.
+
+Implement the Code
+
+function findDup(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    let count = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (array[i] === array[j]) {
+        count += 1;
+      }
+
+      if (count === 2) {
+        return console.log(array[j]);
+      }
+    }
+  }
+}
+
+findDup([1, 5, 3, 1]); // 1
+findDup([
+  18, 9, 36, 96, 31, 19, 54, 75, 42, 15, 38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+  14, 61, 90, 81, 8, 63, 95, 99, 30, 65, 78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+  89, 10, 84, 1, 47, 68, 12, 33, 86, 60, 41, 44, 83, 35, 94, 73, 98, 3, 64, 82,
+  55, 79, 80, 21, 39, 72, 13, 50, 6, 70, 85, 87, 51, 17, 66, 20, 28, 26, 2, 22,
+  40, 23, 71, 62, 73, 32, 43, 24, 4, 56, 7, 34, 57, 74, 45, 11, 88, 67, 5, 58,
+]); // 73
+*/
+
+//5)
