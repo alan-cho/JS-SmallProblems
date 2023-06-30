8a3086845828c79fb40ddddae5f21975f041b320
Alan Cho <alancho397@gmail.com>
2023-06-30 07:11:27 -0500

    Initialize list-processing.js, added solution one

---
+++ Other/list-processing.js
@@ -0,0 +1,18 @@
+// 1)
+/*
+function sum(num) {
+  let string = num.toString();
+  let array = string.split("");
+  let runningTotal = 0;
+  array.forEach(function (number) {
+    runningTotal += Number(number);
+  });
+  console.log(runningTotal);
+}
+
+sum(23); // 5
+sum(496); // 19
+sum(123456789); // 45
+*/
+
+// 2)
