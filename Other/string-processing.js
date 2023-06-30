84435f241b7c8fed4c93c95c999d4d874c1e4713
Alan Cho <alancho397@gmail.com>
2023-06-30 15:51:39 -0500

    Added solution one

--- Other/string-processing.js
+++ Other/string-processing.js
@@ -1,1 +1,15 @@
 // 1)
+/*
+function isUppercase(string) {
+  return console.log(string.toUpperCase() === string);
+}
+
+isUppercase("t"); // false
+isUppercase("T"); // true
+isUppercase("Four Score"); // false
+isUppercase("FOUR SCORE"); // true
+isUppercase("4SCORE!"); // true
+isUppercase(""); // true
+*/
+
+// 2)
