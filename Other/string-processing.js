649351fb1b7c4e4f71f661c6f4776c8c6439ca3d
Alan Cho <alancho397@gmail.com>
2023-06-30 16:45:08 -0500

    Added solution four

--- Other/string-processing.js
+++ Other/string-processing.js
@@ -60,3 +60,22 @@ letterCaseCount(""); // { lowercase: 0, uppercase: 0, neither: 0 }
 */
 
 // 4)
+/*
+function wordCap(string) {
+  let result = string
+    .split(" ")
+    .map(function (word) {
+      word = word.slice(0, 1).toUpperCase() + word.slice(1);
+      return word;
+    })
+    .join(" ");
+
+  console.log(result);
+}
+
+wordCap("four score and seven"); // "Four Score And Seven"
+wordCap("the javaScript language"); // "The Javascript Language"
+wordCap('this is a "quoted" word'); // 'This Is A "quoted" Word'
+*/
+
+// 5)
