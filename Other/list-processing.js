f46446a7fa37b93eaccd8b5a65e6e41565cefe77
Alan Cho <alancho397@gmail.com>
2023-06-30 14:42:16 -0500

    Updated problem set list-processing.js

--- Other/list-processing.js
+++ Other/list-processing.js
@@ -94,3 +94,17 @@ substrings("abcde");
 */
 
 // 6)
+function palindromes(string) {}
+
+palindromes("abcd"); // []
+palindromes("madam"); // [ "madam", "ada" ]
+
+palindromes("hello-madam-did-madam-goodbye");
+// returns
+// [ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+//   "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+//   "-madam-", "madam", "ada", "oo" ]
+
+palindromes("knitting cassettes");
+// returns
+// [ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
