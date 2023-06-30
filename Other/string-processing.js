18163caadef8df710feda261c64a890db02421cd
Alan Cho <alancho397@gmail.com>
2023-06-30 16:20:26 -0500

    Added solution two

--- Other/string-processing.js
+++ Other/string-processing.js
@@ -13,3 +13,26 @@ isUppercase(""); // true
 */
 
 // 2)
+/*
+function removeVowels(array) {
+  let result = [];
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  array.forEach(function (string) {
+    let characters = string.split("");
+    console.log(characters);
+    characters = characters.filter(
+      (char) => !vowels.includes(char.toLowerCase())
+    );
+    result.push(characters.join(""));
+  });
+
+  console.log(result);
+}
+
+removeVowels(["abcdefghijklmnopqrstuvwxyz"]); // ["bcdfghjklmnpqrstvwxyz"]
+removeVowels(["green", "YELLOW", "black", "white"]); // ["grn", "YLLW", "blck", "wht"]
+removeVowels(["ABC", "AEIOU", "XYZ"]); // ["BC", "", "XYZ"]
+*/
+
+// 3)
