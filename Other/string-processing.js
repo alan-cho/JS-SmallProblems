42dfb134e4fa6605bd1272b0d6458cafe5bda07e
Alan Cho <alancho397@gmail.com>
2023-06-30 16:41:40 -0500

    Added solution three

--- Other/string-processing.js
+++ Other/string-processing.js
@@ -36,3 +36,27 @@ removeVowels(["ABC", "AEIOU", "XYZ"]); // ["BC", "", "XYZ"]
 */
 
 // 3)
+/*
+function letterCaseCount(string) {
+  let result = { lowercase: 0, uppercase: 0, neither: 0 };
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i].match(/[^a-z]/i)) {
+      result["neither"] += 1;
+    } else if (string[i] === string[i].toUpperCase()) {
+      result["uppercase"] += 1;
+    } else {
+      result["lowercase"] += 1;
+    }
+  }
+
+  console.log(result);
+}
+
+letterCaseCount("abCdef 123"); // { lowercase: 5, uppercase: 1, neither: 4 }
+letterCaseCount("AbCd +Ef"); // { lowercase: 3, uppercase: 3, neither: 2 }
+letterCaseCount("123"); // { lowercase: 0, uppercase: 0, neither: 3 }
+letterCaseCount(""); // { lowercase: 0, uppercase: 0, neither: 0 }
+*/
+
+// 4)
