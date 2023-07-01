251d2da8ccaf6b60d155061242e74eaf63e73710
Alan Cho <alancho397@gmail.com>
2023-07-01 17:41:09 -0500

    Added solution two

--- Other/problem-solving.js
+++ Other/problem-solving.js
@@ -54,3 +54,65 @@ lightsOn(100); // [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 */
 
 // 2)
+/*
+Understand the Problem
+  Input: Integer
+  Output: Several Logged Strings
+
+  Explicit:
+    The integer will be odd.
+    We must output a four-pointed diamond consisting of '*'s.
+    The rows of the diamond are based on the inputted integer.
+
+  Implicit:
+    The diamond consists of two triangles that are formed together at the base.
+      They share the same base.
+    Each row will contain an odd number of characters.
+    The middle row of the diamond determines the width of the diamond.
+      Which is the inputted integer.
+
+Data Structures
+  String
+
+Algorithms
+  Initialize a string.
+  Initialize the middle row string.
+  For Loop to Create the Top Half
+    Log each row through each iteration.
+  Log the middle row string.
+  For Loop to Create the Bottom Half
+    Log each row through each iteration.
+  
+Implement the Code
+
+function diamond(rows) {
+  let middleRow = "*".repeat(rows);
+  createTopHalf(rows);
+  console.log(middleRow);
+  createBottomHalf(rows);
+}
+
+function createTopHalf(rows) {
+  let starCount = 1;
+  for (i = 1; i <= Math.floor(rows / 2); i += 1) {
+    let row = " ".repeat(Math.ceil(rows / 2) - i) + "*".repeat(starCount);
+    console.log(row);
+    starCount += 2;
+  }
+}
+
+function createBottomHalf(rows) {
+  let starCount = rows - 2;
+  for (let i = Math.floor(rows / 2); i >= 1; i -= 1) {
+    let row = " ".repeat(Math.ceil(rows / 2) - i) + "*".repeat(starCount);
+    console.log(row);
+    starCount -= 2;
+  }
+}
+
+diamond(1);
+diamond(3);
+diamond(9);
+*/
+
+// 3)
