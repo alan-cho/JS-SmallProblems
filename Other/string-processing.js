bb822883050cf0d84eac4417c54122f4667c4ec9
Alan Cho <alancho397@gmail.com>
2023-07-01 07:31:53 -0500

    Added solution six

--- Other/string-processing.js
+++ Other/string-processing.js
@@ -99,3 +99,25 @@ swapCase("Tonight on XYZ-TV"); // "tONIGHT ON xyz-tv"
 */
 
 // 6)
+/*
+function staggeredCase(string) {
+  return console.log(
+    string
+      .split("")
+      .map((char, index) => {
+        if (index % 2 === 0) {
+          return char.toUpperCase();
+        } else {
+          return char.toLowerCase();
+        }
+      })
+      .join("")
+  );
+}
+
+staggeredCase("I Love Launch School!"); // "I LoVe lAuNcH ScHoOl!"
+staggeredCase("ALL_CAPS"); // "AlL_CaPs"
+staggeredCase("ignore 77 the 4444 numbers"); // "IgNoRe 77 ThE 4444 nUmBeRs"
+*/
+
+// 7)
