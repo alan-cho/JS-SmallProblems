7a417c6645f9174bef50fab2c1f6c880a9cddd20
Alan Cho <alancho397@gmail.com>
2023-07-01 08:03:12 -0500

    Added solution two

--- Other/debugging.js
+++ Other/debugging.js
@@ -14,3 +14,65 @@ console.log(ladder); // expect: head-heal-teal-tell-tall-tail
 */
 
 // 2)
+/*
+const RESERVED_KEYWORDS = [
+  "break",
+  "case",
+  "catch",
+  "class",
+  "const",
+  "continue",
+  "debugger",
+  "default",
+  "delete",
+  "do",
+  "else",
+  "enum",
+  "export",
+  "extends",
+  "finally",
+  "for",
+  "function",
+  "if",
+  "implements",
+  "import",
+  "in",
+  "instanceof",
+  "interface",
+  "let",
+  "new",
+  "package",
+  "private",
+  "protected",
+  "public",
+  "return",
+  "static",
+  "super",
+  "switch",
+  "this",
+  "throw",
+  "try",
+  "typeof",
+  "var",
+  "void",
+  "while",
+  "with",
+  "yield",
+];
+
+function isReserved(name) {
+  let result = RESERVED_KEYWORDS.filter((reserved) => {
+    if (name === reserved) {
+      return true;
+    }
+  });
+
+  return result.includes(name);
+}
+
+console.log(isReserved("monkey")); // false
+console.log(isReserved("patch")); // false
+console.log(isReserved("switch")); // should be: true
+*/
+
+// 3)
