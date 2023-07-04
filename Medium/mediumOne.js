3fc09966bc1e82cf27a46d44802f69dffa3331cc
Alan Cho <alancho397@gmail.com>
2023-07-04 01:03:46 -0500

    Added solution nine

--- Medium/mediumOne.js
+++ Medium/mediumOne.js
@@ -366,3 +366,24 @@ fibonacci(75); // 2111485077978050
 */
 
 // 9)
+/*
+let fibonacciObj = {};
+function fibonacci(nth) {
+  if (nth === 1 || nth === 2) {
+    return 1;
+  } else if (fibonacciObj[nth]) {
+    return fibonacciObj[nth];
+  } else {
+    fibonacciObj[nth] = fibonacci(nth - 2) + fibonacci(nth - 1);
+    return fibonacciObj[nth];
+  }
+}
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
+console.log(fibonacci(12)); // 144
+console.log(fibonacci(20)); // 6765
+*/
